4f462b5aecba7e7855065d79314ae00df126234b
adeyinka oluwaseun <seun.adeyinka@gmail.com>
2021-06-17 00:57:26 +0200

    Add project files.

---
+++ WcfServiceDemo/IIpAddressService.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace WcfServiceDemo
+{
+    [ServiceContract]
+    public interface IIpAddressService
+    {
+        [OperationContract]
+        CompositeType GetIpAddress();
+    }
+   
+    [DataContract]
+    public class CompositeType
+    {
+        [DataMember]
+        public string IpAddress
+        {
+            get;
+            set;
+        }
+    }
+    public class IpResponse
+    { 
+        public string Ip { get; set; }
+    }
+}
---
+++ WcfServiceDemo/IpAddressService.svc.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace WcfServiceDemo
+{
+    public class IpAddressService : IIpAddressService
+    {
+        public const string ApiUrl = "https://api.ipify.org/?format=json";
+        public CompositeType GetIpAddress()
+        {
+            using (var client = new WebClient())
+            {
+                var jsonResponse = client.DownloadString(ApiUrl);
+                var response = JsonConvert.
+                    DeserializeObject<IpResponse>(jsonResponse);
+                return new CompositeType() {IpAddress = response.Ip};
+            }
+        }
+    }
+}
